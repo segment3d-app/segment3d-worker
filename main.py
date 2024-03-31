3e6a37d48d861e289e09032c8077dd697072d1bf
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-03-31 19:59:55 +0700

    feat: implement worker queue

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-import logging
 import asyncio
+import logging
+import os
 
 from dotenv import load_dotenv
 
+from aio_pika import connect_robust
+from aio_pika.abc import AbstractIncomingMessage
 
-def main():
-    async def task():
-        logging.info("Running tasks...")
 
-        await asyncio.sleep(5)
-        logging.info("Tasks run successfuly!")
+async def task(message: AbstractIncomingMessage):
+    logging.info("Received message: %s", message.body)
+    logging.info("Running tasks...")
 
-    asyncio.run(task())
+    await asyncio.sleep(10)
+    logging.info("Tasks run successfuly!")
+
+    await message.ack()
+
+
+async def main():
+    connection = await connect_robust(
+        host=os.getenv("RABBITMQ_HOST"),
+        port=int(os.getenv("RABBITMQ_PORT")),
+        login=os.getenv("RABBITMQ_USER"),
+        password=os.getenv("RABBITMQ_PASSWORD"),
+    )
+
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
+
+
+    queue_name = os.getenv("RABBITMQ_QUEUE")
+    queue = await channel.declare_queue(queue_name, durable=True)
+
+    await queue.consume(task)
+
+    try:
+        await asyncio.Future()
+    finally:
+        await connection.close()
 
 
 if __name__ == "__main__":
@@ -20,4 +47,4 @@ if __name__ == "__main__":
     log_format = "%(asctime)s [%(levelname)s]: (%(name)s) %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_format)
 
-    main()
+    asyncio.run(main())
