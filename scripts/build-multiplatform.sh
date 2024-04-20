83225a928ecd5fad6008ac7fe5b9a25cd9ef0153
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-04-20 13:35:30 +0700

    feat: update multi arch builder

--- scripts/build-multiplatform.sh
+++ scripts/build-multiplatform.sh
@@ -3,11 +3,18 @@
 # Exit script on error
 set -e
 
-# Step 1: Setup multiplatform Docker builder
-echo "Setting up multiplatform Docker builder..."
-docker buildx create --name multiarch-builder --driver docker-container --use
+# Check if the multiarch builder exists
+if ! docker buildx ls | grep -q multiarch-builder; then
+  echo "Creating new multiarch builder..."
+  docker buildx create --name multiarch-builder --driver docker-container --use
+else
+  echo "Using existing multiarch builder..."
+  docker buildx use multiarch-builder
+fi
+
+# Ensure the builder is running
 docker buildx inspect --bootstrap
 
-# Step 2: Build and push Docker image
-echo "Building and pushing Docker image to Docker Hub..."
+# Build and push Docker image for multiple platforms
+echo "Building and pushing Docker image for multiple platforms..."
 docker buildx build --platform linux/amd64,linux/arm64 -t marcellinoco/segment3d:segment3d-worker --push .
